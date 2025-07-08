b552b67a07beae65ed4d06a53022e5a1a6de6bbd
rishav-jha-mech <rishav.rj74@gmail.com>
2025-07-08 13:13:20 +0530

    Drawer story updated

--- src/stories/components/drawer.stories.tsx
+++ src/stories/components/drawer.stories.tsx
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { useCallback, useMemo, useState } from "react";
 import Drawer from "../../components/drawer/drawer";
-import type { DrawerBtnProps } from "../../components/drawer/drawer-types";
-import type { DropdownItemProps } from "../../components/dropdown/dropdown-types";
+import { DrawerProvider } from "../../context/drawer-context/drawer-context";
+import { Text } from "../../components/text/text";
 import FlexView from "../../components/flex-view/flex-view";
 
 const meta: Meta<typeof Drawer> = {
@@ -18,91 +17,22 @@ export default meta;
 
 type Story = StoryObj<typeof Drawer>;
 
-const DrawerStory = () => {
-  const [selectedOption, setSelectedOption] = useState("dbservices");
-  const [drawerOpen, setDrawerOpen] = useState(true);
-
-  const handleToggleDrawer = useCallback(() => {
-    setDrawerOpen((prev) => !prev);
-  }, []);
-
-  const dropdownOptions: DropdownItemProps[] = useMemo(
-    () => [
-      { label: "DB Services", value: "dbservices" },
-      { label: "PostgreSQL", value: "postgresql" },
-      { label: "MySQL", value: "mysql" },
-      { label: "MongoDB", value: "mongodb" },
-      { label: "Redis", value: "redis" },
-      { label: "SQLite", value: "sqlite" },
-    ],
-    []
-  );
-  const handleDropdownChange = useCallback((value: string | number) => {
-    setSelectedOption(value as string);
-  }, []);
-
-  const menuOptions: DrawerBtnProps[] = [
-    {
-      $icon: "MyServices",
-      $isSelected: true,
-      label: "My Services",
-    },
-    {
-      $icon: "Provisioning",
-      label: "Provisioning",
-    },
-    {
-      $icon: "AvailabilityMachines",
-      label: "Availability Machines",
-    },
-    {
-      $icon: "DataFlix",
-      label: "Dataflix",
-    },
-    {
-      $icon: "ScriptLibrary",
-      label: "ScriptLibrary",
-    },
-    {
-      $icon: "Benchmarks",
-      label: "Benchmarks",
-    },
-    {
-      $icon: "Servers",
-      label: "Servers",
-    },
-  ];
-
-  const footerOptions: DrawerBtnProps[] = [
-    {
-      $icon: "Add",
-      label: "Invite People",
-      $trailingIcon: "Pin",
-    },
-    {
-      $icon: "Book",
-      label: "Help & Support",
-      $trailingIcon: "Pin",
-    },
-  ];
-
-  return (
-    <FlexView>
-      <Drawer
-        isOpen={drawerOpen}
-        toggleDrawer={handleToggleDrawer}
-        dropdown={{
-          options: dropdownOptions,
-          selected: selectedOption,
-          handleChange: handleDropdownChange,
-        }}
-        menuOptions={menuOptions}
-        footerOptions={footerOptions}
-      />
-    </FlexView>
-  );
-};
-
 export const Default: Story = {
-  render: () => <DrawerStory />,
+  render: () => (
+    <DrawerProvider>
+      <FlexView
+        $paddingHorizontal={20}
+        $paddingVertical={20}
+        $direction="column"
+      >
+        <Text $renderAs="heading/Titles">Content Area</Text>
+        <Text $renderAs="bodyPrimary" $color="subtler">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab in
+          corrupti magnam, ipsa illum sed illo architecto eveniet explicabo
+          dolorum, corporis similique officiis laudantium, tempore velit impedit
+          aut consequatur tenetur.
+        </Text>
+      </FlexView>
+    </DrawerProvider>
+  ),
 };
